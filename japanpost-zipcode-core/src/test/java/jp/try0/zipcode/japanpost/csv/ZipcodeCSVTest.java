d1e4c5cd35c327aa4aebe7f611157136c9427ac0
Ryo <try0.development@gmail.com>
2024-01-28 19:35:42 +0900

    serialize確認

--- japanpost-zipcode-core/src/test/java/jp/try0/zipcode/japanpost/csv/ZipcodeCSVTest.java
+++ japanpost-zipcode-core/src/test/java/jp/try0/zipcode/japanpost/csv/ZipcodeCSVTest.java
@@ -1,11 +1,14 @@
 package jp.try0.zipcode.japanpost.csv;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.nio.file.Files;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 
 import jp.try0.zipcode.japanpost.util.ZipcodeFiles;
@@ -44,5 +47,19 @@ public class ZipcodeCSVTest {
 
 		Assertions.assertFalse(csv.getFile().exists(), "削除できない");
 
+	}
+
+	@Test
+	public void serializeTest() throws Exception {
+		ZipcodeCSV csv = ZipcodeCSV.getOrDownload();
+		serialize(csv);
+	}
+
+	private byte[] serialize(Object object) throws IOException {
+		try (ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+				ObjectOutputStream outputStream = new ObjectOutputStream(byteStream);) {
+			outputStream.writeObject(object);
+			return byteStream.toByteArray();
+		}
 	}
 }
